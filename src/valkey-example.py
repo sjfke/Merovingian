7b34f39e1631bc0d053c230c87ffbd75377b1bb6
Geoffrey Collis <geoffreycollis@hotmail.com>
2024-12-10 13:03:33 +0100

    add decode_responses

--- src/valkey-example.py
+++ src/valkey-example.py
@@ -5,19 +5,18 @@ import valkey
 # connection.client_info()  # Client Information as a dictionary
 # connection.close()        # Does nothing
 
-connection = valkey.Valkey(host='localhost', port=6379, db=0)
+connection = valkey.Valkey(host='localhost', port=6379, db=0, decode_responses=True)
 
 # List
 connection.delete("names")
 connection.lpush("names", "Ashley")                 # 1, new length
 connection.rpush("names", "Barry", "Christina")     # 3, new length
-connection.lrange("names", 0, -1)                     # [b'Ashley', b'Barry', b'Christina']
-connection.lindex("names", 0)                        # b'Ashley' (bytes)
-print(connection.lindex("names", 0).decode("utf-8")) # Ashley (str)
-connection.lpop("names")                                         # b'Ashley' (bytes)
-connection.lrange("names", 0, -1)                     # [b'Barry', b'Christina']
-connection.rpop("names")                                         # b'Christina'
-connection.lrange("names", 0, -1)                     # [b'Barry']
+connection.lrange("names", 0, -1)                     # ['Ashley', 'Barry', 'Christina']
+connection.lindex("names", 0)                        # 'Ashley'
+connection.lpop("names")                                         # 'Ashley'
+connection.lrange("names", 0, -1)                     # ['Barry', 'Christina']
+connection.rpop("names")                                         # 'Christina'
+connection.lrange("names", 0, -1)                     # ['Barry']
 connection.delete("names")                                       # 1 Successful
 connection.llen("names")                                         # 0, new length
 
@@ -26,7 +25,7 @@ connection.delete("names")
 connection.hset("names", "Ashley", 1)   # 1
 connection.hset("names", "Barry", 2)    # 1
 connection.hset("names", "Cristina", 3) # 1
-connection.hgetall("names")                              # {b'Ashley': b'1', b'Barry': b'2', b'Cristina': b'3'}
+connection.hgetall("names")                              # {'Ashley': '1', 'Barry': '2', 'Cristina': '3'}
 connection.hexists("names", "Ashley")         # True
 connection.hexists("names", "Barley")         # False
 # connection.close()                                    # Does nothing
